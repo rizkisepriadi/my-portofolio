3bf0536e31627128b82447cd68bf8e46ce50786d
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2023-12-31 21:02:50 +0700

    React 1.0

---
+++ src/Pages/Skill.js
@@ -0,0 +1,14 @@
+export default function Skill() {
+  return (
+    <article className="skill" id="skill">
+      <h1>Skill</h1>
+      <ul>
+        <li>OOP Java</li>
+        <li>HTML & CSS</li>
+        <li>Basic JavaScript</li>
+        <li>Basic Phyton</li>
+        <li>Design UI with Figma</li>
+      </ul>
+    </article>
+  );
+}
