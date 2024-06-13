beec930c2872acf0d782cc1a57955de1d068dc50
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-06-13 21:54:31 +0700

    New UI

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-portofolio/', // Ganti 'my-portofolio' dengan nama repository Anda
+  base: '/my-portofolio/',  // Sesuaikan dengan nama repository atau path aplikasi Anda
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',  // Pastikan output directory sesuai dengan yang diharapkan oleh Netlify
   },
 });
